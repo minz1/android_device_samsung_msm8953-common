e9f813441c3ec40bb3f712dc443d4cc1521c66fa
minz1 <emerytang@gmail.com>
2017-05-26 23:02:48 -0400

    msm8953-common: initial skeleton tree

---
+++ device.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+COMMON_PATH := device/samsung/msm8916-common
+
+# Inherit from common
+$(call inherit-product-if-exists, device/samsung/qcom-common/qcom-common.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Include package config fragments
+include $(COMMON_PATH)/product/*.mk
+
+# Include proprietary blobs
+$(call inherit-product, vendor/samsung/msm8953-common/msm8953-common-vendor.mk)
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
