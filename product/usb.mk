055fea70fa8f63042b1fb49206a9f785ced1c225
minz1 <emerytang@gmail.com>
2017-11-01 15:10:13 -0500

    product: usb: build USB HAL

--- product/usb.mk
+++ product/usb.mk
@@ -1,3 +1,7 @@
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
