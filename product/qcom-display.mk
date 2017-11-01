677a819ae7f56df9b3e2e863c03bd73b55a35534
minz1 <emerytang@gmail.com>
2017-11-01 14:57:27 -0500

    product: qcom-display: update display/render for O

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -6,7 +6,13 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     liboverlay \
-    libqdMetaData
+    libqdMetaData \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    libgenlock
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
@@ -16,3 +22,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
