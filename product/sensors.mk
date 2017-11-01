9e53896483d85af7c369ebc187d04d944a90ff52
minz1 <emerytang@gmail.com>
2017-11-01 15:22:44 -0500

    product: sensors: build Sensors HAL

--- product/sensors.mk
+++ product/sensors.mk
@@ -1,7 +1,11 @@
+# Sensors HAL
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml
 
 # Sensors
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf
+    $(COMMON_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
