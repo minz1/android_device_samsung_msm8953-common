e8283a107a98667f1d45f7bcf3eabea92bfd4f4d
minz1 <emerytang@gmail.com>
2017-11-01 15:15:23 -0500

    board: qcom-display: update display flags

--- board/qcom-display.mk
+++ board/qcom-display.mk
@@ -5,6 +5,9 @@ TARGET_CONTINUOUS_SPLASH_ENABLED := true
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
 USE_OPENGL_RENDERER := true
+TARGET_USES_GRALLOC1 := true
+TARGET_USES_HWC2 := true
+TARGET_USES_OVERLAY := true
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
