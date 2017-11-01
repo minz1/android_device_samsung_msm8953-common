3e84e58b4567cecc38e5641c7ffcb772c632b830
minz1 <emerytang@gmail.com>
2017-11-01 14:55:33 -0500

    product: qcom-bluetooth: build Bluetooth HAL

--- product/qcom-bluetooth.mk
+++ product/qcom-bluetooth.mk
@@ -3,6 +3,10 @@ PRODUCT_PACKAGES += \
     javax.btobex \
     libbt-vendor
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
