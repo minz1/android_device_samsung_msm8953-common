8f2af5f68663536076230e94f6d09dee18d272b8
minz1 <emerytang@gmail.com>
2017-11-12 22:36:32 -0600

    ramdisk: update
    * Various changes, most notably pulling init.target.rc from the stock J7 Perx ROM

--- product/init.mk
+++ product/init.mk
@@ -5,9 +5,11 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.target.rc \
     ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
     init.qcom.fm.sh \
-    init.qcom.post_boot.sh
+    init.qcom.post_boot.sh \
+    init.qti.ims.sh
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -43,6 +43,14 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.target.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= init.target.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.qcom.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
@@ -71,5 +79,13 @@ LOCAL_MODULE		:= init.qcom.post_boot.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.post_boot.sh
+LOCAL_MODULE_PATH	:= $(TARGET_OUT)/etc
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.qti.ims.sh
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.qti.ims.sh
 LOCAL_MODULE_PATH	:= $(TARGET_OUT)/etc
 include $(BUILD_PREBUILT)
