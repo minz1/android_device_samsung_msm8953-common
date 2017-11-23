2402db817641ae04a2be17e706366c3c1e371d0c
minz1 <emerytang@gmail.com>
2017-11-23 09:18:18 -0600

    ramdisk: fix tabs in makefile

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,97 +3,97 @@ LOCAL_PATH := $(call my-dir)
 # Device init scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= fstab.qcom
+LOCAL_MODULE            := fstab.qcom
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= fstab.qcom
+LOCAL_SRC_FILES	        := fstab.qcom
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.qcom.rc
+LOCAL_MODULE            := init.qcom.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= init.qcom.rc
+LOCAL_SRC_FILES	        := init.qcom.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.qcom.sh
+LOCAL_MODULE            := init.qcom.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= init.qcom.sh
+LOCAL_SRC_FILES	        := init.qcom.sh
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.qcom.usb.rc
+LOCAL_MODULE            := init.qcom.usb.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= init.qcom.usb.rc
+LOCAL_SRC_FILES	        := init.qcom.usb.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.qcom.usb.sh
+LOCAL_MODULE            := init.qcom.usb.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= init.qcom.usb.sh
+LOCAL_SRC_FILES	        := init.qcom.usb.sh
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.target.rc
+LOCAL_MODULE            := init.target.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= init.target.rc
+LOCAL_SRC_FILES	        := init.target.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= ueventd.qcom.rc
+LOCAL_MODULE            := ueventd.qcom.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= ueventd.qcom.rc
+LOCAL_SRC_FILES	        := ueventd.qcom.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.qcom.bt.sh
+LOCAL_MODULE            := init.qcom.bt.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.qcom.bt.sh
+LOCAL_SRC_FILES	        := etc/init.qcom.bt.sh
 LOCAL_MODULE_PATH	:= $(TARGET_OUT)/etc
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.qcom.fm.sh
+LOCAL_MODULE            := init.qcom.fm.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.qcom.fm.sh
+LOCAL_SRC_FILES	        := etc/init.qcom.fm.sh
 LOCAL_MODULE_PATH	:= $(TARGET_OUT)/etc
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.qcom.post_boot.sh
+LOCAL_MODULE            := init.qcom.post_boot.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.qcom.post_boot.sh
+LOCAL_SRC_FILES	        := etc/init.qcom.post_boot.sh
 LOCAL_MODULE_PATH	:= $(TARGET_OUT)/etc
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.qcom.uicc.sh
+LOCAL_MODULE            := init.qcom.uicc.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.qcom.uicc.sh
+LOCAL_SRC_FILES	        := etc/init.qcom.uicc.sh
 LOCAL_MODULE_PATH	:= $(TARGET_OUT)/etc
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.qti.ims.sh
+LOCAL_MODULE            := init.qti.ims.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.qti.ims.sh
+LOCAL_SRC_FILES	        := etc/init.qti.ims.sh
 LOCAL_MODULE_PATH	:= $(TARGET_OUT)/etc
 include $(BUILD_PREBUILT)
