dbcac60db3d7e5a856e612f8130f1dfaa1d2ba31
minz1 <emerytang@gmail.com>
2017-11-24 21:51:26 -0600

    msm8953-common: add protobuf support

--- board/ril.mk
+++ board/ril.mk
@@ -1,3 +1,6 @@
 # RIL
 BOARD_RIL_CLASS := ../../../device/samsung/msm8953-common/ril
 BOARD_PROVIDES_LIBRIL := true
+
+# Protobuf
+PROTOBUF_SUPPORTED := true
--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -32,6 +32,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full
+
 # Security configuration file
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
