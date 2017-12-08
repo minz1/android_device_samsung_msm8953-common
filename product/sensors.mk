7c938230e363212660360a04478b07a519917001
minz1 <emerytang@gmail.com>
2017-12-08 12:16:42 -0600

    configs: remove hals.conf
    * Should stay in device specific/common trees

--- product/sensors.mk
+++ product/sensors.mk
@@ -5,7 +5,3 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml
-
-# Sensors
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
