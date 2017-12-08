c565eaea0d0a7a12712f58bf2dc87c40dc677625
minz1 <emerytang@gmail.com>
2017-12-08 02:01:39 -0600

    msm8953-common: update media configs from CAF
    * Tag LA.UM.6.6.r1-04400-89xx.0

--- product/qcom-media.mk
+++ product/qcom-media.mk
@@ -1,7 +1,6 @@
 # Media
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
-    $(COMMON_PATH)/configs/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(COMMON_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
 
 PRODUCT_COPY_FILES += \
