9ee4b4fe560e1697aa113fb8790c7065fa5921c8
minz1 <emerytang@gmail.com>
2017-12-09 13:06:04 -0600

    msm8953-common: fix seccomp policy filenames

--- product/seccomp.mk
+++ product/seccomp.mk
@@ -1,4 +1,4 @@
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/seccomp_policy/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec-seccomp.policy \
-    $(COMMON_PATH)/seccomp_policy/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor-seccomp.policy
+    $(COMMON_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(COMMON_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
