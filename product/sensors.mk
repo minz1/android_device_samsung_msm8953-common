7f6f60c640e85c231f4aeb66064432e11e492289
minz1 <emerytang@gmail.com>
2017-12-18 06:26:14 -0600

    msm8953-common: remove hals.conf

--- product/sensors.mk
+++ product/sensors.mk
@@ -1,7 +1,3 @@
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml
-
-# Sensors
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf
