e418073bdb2546271b5e43425dce7811658557f8
Wzedlare <vedatak01@gmail.com>
2018-01-12 08:17:46 -0600

    rootdir: use c7lte ramdisk

--- product/init.mk
+++ product/init.mk
@@ -1,16 +1,25 @@
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.carrier.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.rilchip.rc \
+    init.samsung.rc \
     init.target.rc \
-    ueventd.qcom.rc
-
-PRODUCT_PACKAGES += \
+    init.wifi.rc \
+    ueventd.qcom.rc \
+    control_netmgrd.sh \
+    hsic.control.bt.sh \
+    init.ath3k.bt.sh \
+    init.crda.sh \
     init.qcom.bt.sh \
+    init.qcom.coex.sh \
+    init.qcom.efs.sync.sh \
     init.qcom.fm.sh \
     init.qcom.post_boot.sh \
-    init.qcom.uicc.sh \
-    init.qti.ims.sh
+    init.qcom.wifi.sh \
+    init.qti.ims.sh \
+    qca6234-service.sh
