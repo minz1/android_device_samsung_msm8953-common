7f200e8a752afbd961b2fadc2837e9537d4a5a42
minz1 <emerytang@gmail.com>
2018-01-12 11:35:32 -0600

    product: qcom-power: add QPerformance to boot jars

--- product/qcom-power.mk
+++ product/qcom-power.mk
@@ -2,6 +2,9 @@
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
 
+# QPerformance
+PRODUCT_BOOT_JARS += QPerformance
+
 # Power HAL
 PRODUCT_PACKAGES += \
     power.qcom
