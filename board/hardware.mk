1ddf286c74421f0cdc05512a0f20682ebceeede6
minz1 <emerytang@gmail.com>
2018-01-23 21:15:24 -0600

    cmhw: add mdnie display calibration

--- board/hardware.mk
+++ board/hardware.mk
@@ -2,5 +2,5 @@
 BOARD_USES_CYANOGEN_HARDWARE := true
 BOARD_HARDWARE_CLASS +=	\
 	hardware/cyanogen/cmhw \
-	hardware/samsung/cmhw #\
-	#$(PLATFORM_PATH)/cmhw
+	hardware/samsung/cmhw \
+	$(COMMON_PATH)/cmhw
