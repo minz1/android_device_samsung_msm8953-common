0f922f851d823df4af45ba6c3a92c341de7ae499
Sean Hoyt <seanhoyt963@gmail.com>
2018-06-28 00:07:29 -0400

    Force enable dexpreopt
    Change-Id: Ic8e35643caab9949262f1fb7f5177ed7dc11301c

--- board/art.mk
+++ board/art.mk
@@ -1,9 +1,3 @@
 # ART
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
+WITH_DEXPREOPT := true
 WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
